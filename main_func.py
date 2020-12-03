552a9b9b2776265b6d95180d2e203a08d17a7b08
kerembyl <you@example.com>
2020-12-03 19:13:28 +0300

    Regular Expression ile deyimler, harfler ve özel işaretli kelime öbekleri filtrelendi

--- main_func.py
+++ main_func.py
@@ -1,17 +1,32 @@
-#Turk Dil Kurumu scraper.
+#Turk Dil Kurumu crawler.
 #Sitedeki tüm kelimeleri alır, .txt dosyasına yazar.
+
 import requests
+import re
 
 url = 'https://sozluk.gov.tr/autocomplete.json'
 uh = requests.get(url)
-fh = open('output.txt', 'w', encoding='utf-8')
 if not uh.status_code == 200: raise Exception('Siteye baglanilamadi :(')
+fh = open('output.txt', 'w', encoding='utf-8')
+
+# Apply filters to filter out non-word content. Set False to disable.
+filters = True 
+
+def stringCheck (word=str):
+    if filters:
+        if re.search("^[a-z][a-z]",word) and not re.search("[a-z]\s", word):
+            return True
+        else:
+            return False
+    else: return True
+
 
 data = uh.json()
 for i in data:
     word = i.get('madde')
-    
-    fh.write(word)
-    fh.write('\n') # Insert new line
-
+    if stringCheck(word):
+        fh.write(word)
+        fh.write('\n') # Insert new line
 fh.close()
+
+
