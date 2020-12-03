e8208563feeb72ac3435152963452373212356b5
kerembyl <you@example.com>
2020-12-03 16:20:45 +0300

    Ana fonksiyon oluşturuldu ve output.txt dosyası eklendi.

--- main_func.py
+++ main_func.py
@@ -1,2 +1,18 @@
 #Turk Dil Kurumu crawler.
 #Sitedeki tüm kelimeleri alır, .txt dosyasına yazar.
+import requests
+import os
+
+url = 'https://sozluk.gov.tr/autocomplete.json'
+uh = requests.get(url)
+fh = open('output.txt', 'w', encoding='utf-8')
+if not uh.status_code == 200: raise Exception('Siteye baglanilamadi :(')
+
+data = uh.json()
+for i in data:
+    word = i.get('madde')
+    
+    fh.write(word)
+    fh.write('\n')
+
+fh.close()
